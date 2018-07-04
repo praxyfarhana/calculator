7099a2ae8503cc31c30c4ae96366aa4d41da8367
praxyfarhana <praxyfarhana@gmail.com>
2018-07-04 11:59:27 +0300

    created files

---
+++ js/sripts.js
@@ -0,0 +1,9 @@
+var add = function(number1, number2) {
+  return number1 + number2
+};
+alert(add(10,5));
+var number1 = parseInt(prompt("enter a number"));
+alert(number1);
+var number2 = parseInt(prompt("enter a number"));
+alert (number2);
+alert(add(number1, number2));
